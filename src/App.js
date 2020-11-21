c80aab440e4bb2171174595f86bc787258074c13
Shuvo <shuvojyoti.golder@gmail.com>
2020-11-21 12:23:50 +0600

    Header Completed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from Hulu</h1>
+    <div className="app">
       <Header />
       {/* Header */}
 
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Header.css";
 import HomeIcon from "@material-ui/icons/Home";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
 import LiveTvIcon from "@material-ui/icons/LiveTv";
@@ -9,18 +10,36 @@ import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 function Header() {
   return (
     <div className="header">
-      <div className="header-icons">
-        <HomeIcon />
-        <FlashOnIcon />
-        <LiveTvIcon />
-        <VideoLibraryIcon />
-        <SearchIcon />
-        <PersonOutlineIcon />
-        <img
-          src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
-          alt="hulu"
-        ></img>
+      <div className="header_icons">
+        <div className="header_icon header_icon-active">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+        <div className="header_icon">
+          <FlashOnIcon />
+          <p>Trending</p>
+        </div>
+        <div className="header_icon">
+          <LiveTvIcon />
+          <p>Verified</p>
+        </div>
+        <div className="header_icon">
+          <VideoLibraryIcon />
+          <p>Collection</p>
+        </div>
+        <div className="header_icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+        <div className="header_icon">
+          <PersonOutlineIcon />
+          <p>Account</p>
+        </div>
       </div>
+      <img
+        src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
+        alt="hulu"
+      ></img>
     </div>
   );
 }
